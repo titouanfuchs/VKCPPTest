9c728ccb5e60d7bf20e064fdfc41a70fffc31884
Titouan FUCHS <developper@titouanfuchs.fr>
2024-11-12 22:46:10 +0100

    stagin buffer working

--- TriangleApp/TriangleAppMain.cpp
+++ TriangleApp/TriangleAppMain.cpp
@@ -837,6 +837,9 @@ namespace TriangleApp {
 
         createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, VKVertexBuffer, VKVertexBufferMemory);
 
+        copyBuffer(stagingBuffer, VKVertexBuffer, bufferSize);
+        vkDestroyBuffer(VKDevice, stagingBuffer, nullptr);
+        vkFreeMemory(VKDevice, stagingBufferMemory, nullptr);
     }
 
     void TriangleAppMain::createBuffer(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties,
@@ -869,7 +872,37 @@ namespace TriangleApp {
 #pragma region Buffers
 
     void TriangleAppMain::copyBuffer(VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size) {
+        VkCommandBufferAllocateInfo allocInfo{};
+        allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+        allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+        allocInfo.commandPool = VKCommandPool;
+        allocInfo.commandBufferCount = 1;
 
+        VkCommandBuffer commandBuffer;
+        vkAllocateCommandBuffers(VKDevice, &allocInfo, &commandBuffer);
+
+        VkCommandBufferBeginInfo beginInfo{};
+        beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+        beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+
+        vkBeginCommandBuffer(commandBuffer, &beginInfo);
+
+        VkBufferCopy copyRegion{};
+        copyRegion.srcOffset = 0;
+        copyRegion.dstOffset = 0;
+        copyRegion.size = size;
+        vkCmdCopyBuffer(commandBuffer, srcBuffer, dstBuffer, 1, &copyRegion);
+
+        vkEndCommandBuffer(commandBuffer);
+
+        VkSubmitInfo submitInfo{};
+        submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+        submitInfo.commandBufferCount = 1;
+        submitInfo.pCommandBuffers = &commandBuffer;
+
+        vkQueueSubmit(VKGraphicsQueue, 1, &submitInfo, VK_NULL_HANDLE);
+        vkQueueWaitIdle(VKGraphicsQueue);
+        vkFreeCommandBuffers(VKDevice, VKCommandPool, 1, &commandBuffer);
     }
 
 #pragma endregion
